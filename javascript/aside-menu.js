349396dc939d2f6ff01a590d51ae038cb7e7bcb7
xzakm3 <zakyz.martin@gmail.com>
2018-03-22 15:24:48 +0100

    FEATURE: content of page is responsive. Aside menu in progress

--- javascript/aside-menu.js
+++ javascript/aside-menu.js
@@ -3,4 +3,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var element = document.querySelector("#asideNavbarSupportedContent");
         element.classList.add("show");
     }
+
+    test.addEventListener("mouseover", function( event ) {   
+        // highlight the mouseover target
+        event.target.style.color = "orange";
+    
+        // reset the color after a short delay
+        setTimeout(function() {
+          event.target.style.color = "";
+        }, 500);
+      }, false);
 });
+
